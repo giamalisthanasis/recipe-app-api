423bb763e56b0993ca9093c74ed2f56404cbae65
Athanasios Giamalis <giamalisthanasis@hotmail.com>
2023-07-25 01:03:40 +0300

    postgress configuration Signed-off-by: Athanasios Giamalis <giamalisthanasis@hotmail.com>

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator
+"""
+
+def add(x,y):
+    """add x and y and return result."""
+    return x + y
+
+def subtract(x,y):
+    """subtract x from y return result."""
+    return y - x
---
+++ app/app/test.py
@@ -0,0 +1,19 @@
+"""
+Sample test
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+    def test_add_number(self):
+        res=calc.add(5, 6)
+
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        """Test subtract number"""
+        res=calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
