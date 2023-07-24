810a4900be611ed5aba98889de6d6eb1a087f97c
Athanasios Giamalis <giamalisthanasis@hotmail.com>
2023-07-25 01:15:11 +0300

    postgress configuration Signed-off-by: Athanasios Giamalis <giamalisthanasis@hotmail.com>

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator
 """
 
-def add(x,y):
+
+def add(x, y):
     """add x and y and return result."""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtract x from y return result."""
     return y - x
