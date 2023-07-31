3b2efcc2de4eb18d33743a4126fe95d0e798aec0
Athanasios Giamalis <giamalisthanasis@hotmail.com>
2023-07-31 08:40:08 +0300

    wait_for_db and check flake8
    Signed-off-by: Athanasios Giamalis <giamalisthanasis@hotmail.com>

--- app/app/test.py
+++ app/app/test.py
@@ -2,18 +2,18 @@
 Sample test
 """
 from django.test import SimpleTestCase
-
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
     def test_add_number(self):
-        res=calc.add(5, 6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtract number"""
-        res=calc.subtract(10, 15)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
