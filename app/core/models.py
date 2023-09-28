cd8d5465c2397ecff8404f23cee6a7ebed342508
Athanasios Giamalis <giamalisthanasis@hotmail.com>
2023-09-28 04:01:35 +0300

    complete image upload api
    Signed-off-by: Athanasios Giamalis <giamalisthanasis@hotmail.com>

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,8 @@
 """
 Database models.
 """
+import uuid
+import os
 
 from django.conf import settings
 from django.db import models
@@ -11,7 +13,16 @@ from django.contrib.auth.models import (
     )
 
 
+def recipe_image_file_path(instance, filemane):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filemane)[1]  # extract the extension of the file
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
+
+
 class UserManager(BaseUserManager):
+
     """Manager for the users."""
 
     def create_user(self, email, password=None, **extra_fields):
@@ -58,6 +69,9 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+    # django documentation
 
     def __str__(self):
         return self.title
@@ -65,6 +79,18 @@ class Recipe(models.Model):
 
 class Tag(models.Model):
     """Tag for filtering recipes"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient object"""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
